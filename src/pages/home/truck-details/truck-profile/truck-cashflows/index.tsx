a2e24a19d6e3f80ed63bda86859d791dd988803b
sevara-s <nematullayevasevara6@gmail.com>
2025-12-24 16:17:01 +0500

    table add done

--- src/pages/home/truck-details/truck-profile/truck-cashflows/index.tsx
+++ src/pages/home/truck-details/truck-profile/truck-cashflows/index.tsx
@@ -5,15 +5,18 @@ import { VEHICLES_CASHFLOWS } from "@/constants/api-endpoints"
 import { useGet } from "@/hooks/useGet"
 import { useModal } from "@/hooks/useModal"
 import { useGlobalStore } from "@/store/global-store"
+import { useParams } from "@tanstack/react-router"
 import AddVehicleCashflowModal from "./add-cashflows"
 import { useColumnsCashflowsTable } from "./cashflows-cols"
+import TableTruckHeader from "./truck-header"
 
 const VehicleCashflows = () => {
+    const params = useParams({ strict: false })
+    const id = params.id
     const { data, isLoading } =
         useGet<ListResponse<VehicleCashflowsType>>(VEHICLES_CASHFLOWS)
     const { getData, setData } = useGlobalStore()
     const item = getData<VehicleCashflowsType>(VEHICLES_CASHFLOWS)
-
     const { openModal: openDeleteModal } = useModal("delete-vehicle-cashflow")
     const { openModal: openCreateModal } = useModal(`create-vehicle-cashflow`)
     const columns = useColumnsCashflowsTable()
@@ -34,6 +37,16 @@ const VehicleCashflows = () => {
                 data={data?.results}
                 onDelete={handleDelete}
                 onEdit={({ original }) => handleEdit(original)}
+                head={
+                    <TableTruckHeader
+                        storeKey={VEHICLES_CASHFLOWS}
+                        modalKey="create-vehicle-cashflow"
+                        disabled={!id}
+                        pageKey="page"
+                        name="vehicle-cashflow"
+                        searchKey="cashflow_search"
+                    />
+                }
             />
             <DeleteModal
                 path={VEHICLES_CASHFLOWS}
